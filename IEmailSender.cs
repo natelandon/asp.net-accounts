ded3eaf728d8d0cc5f0af3e6fe21a0469c1028a2
Nate Landon <druidprogramming@gmail.com>
2018-01-21 21:42:01 -0700

    Add files via upload

---
+++ IEmailSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Foodie_Queue.Services
+{
+    public interface IEmailSender
+    {
+        Task SendEmailAsync(string email, string subject, string message);
+    }
+}
